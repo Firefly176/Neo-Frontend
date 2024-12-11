96672ce805bb55dd8180366fab2f11cb284c1967
Aniket Gupta <aniket17112000@gmail.com>
2024-12-11 19:07:26 +0530

    initial commit, basic vite + react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
