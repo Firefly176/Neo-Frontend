5537ad02329f594c49a6fb45d327c424bb35c74e
Aniket Gupta <aniket17112000@gmail.com>
2024-12-15 18:36:19 +0530

    feat : Home updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,32 @@
+// import { Calendar } from "@nextui-org/react";
+// import FullCalendar from "@fullcalendar/react";
+import { CalendarComponent } from "../components/Calender";
+import { TransferForm } from "../components/Form";
+import { Card, CardBody } from "@nextui-org/react";
+import { useState } from "react";
+
 function Home() {
+  // eslint-disable-next-line no-unused-vars
+  const [calendarData, setCalendarData] = useState([
+    {
+      title: "Meeting",
+      start: "2024-12-15T11:15:30.762Z",
+      fromAddress: "",
+      toAddress: "",
+      amount: "",
+    },
+  ]);
+
   return (
     <>
-      <h1>hello</h1>
+      <div className="m-auto mt-5 w-[1440px]">
+        <Card>
+          <CardBody>
+            <CalendarComponent data={calendarData} />
+          </CardBody>
+        </Card>
+        <TransferForm />
+      </div>
     </>
   );
 }
