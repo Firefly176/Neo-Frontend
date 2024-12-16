cd1a0d67ad98011ba6a7cc6f3ae14995dbe8f09c
Aniket Gupta <aniket17112000@gmail.com>
2024-12-17 01:40:02 +0530

    feat : improved form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,71 +1,96 @@
-import { useState } from "react";
-import {
-  Form,
-  Input,
-  Button,
-  Card,
-  CardBody,
-  TimeInput,
-} from "@nextui-org/react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import { Form, Input, Button, TimeInput } from "@nextui-org/react";
+import { Time } from "@internationalized/date";
 
-export function TransferForm() {
+const TransferForm = ({ eventData }) => {
+  const [senderAddress, setSenderAddress] = useState(
+    eventData?.event?.fromAddress || "",
+  );
+  const [receiverAddress, setReceiverAddress] = useState(
+    eventData?.event?.toAddress || "",
+  );
+  const [amount, setAmount] = useState(eventData?.event?.amount || "");
+  const [scheduleTime, setScheduleTime] = useState(
+    new Time(
+      new Date(eventData?.event?.start).getHours(),
+      new Date(eventData?.event?.start).getMinutes(),
+    ),
+  );
+
   // eslint-disable-next-line no-unused-vars
   const [action, setAction] = useState(null);
 
   return (
-    <Card className="m-auto w-full max-w-xs flex flex-col gap-4">
-      <CardBody className="p-5">
-        {" "}
-        <Form
-          validationBehavior="native"
-          onReset={() => setAction("reset")}
-          onSubmit={(e) => {
-            e.preventDefault();
-            let data = Object.fromEntries(new FormData(e.currentTarget));
+    <Form
+      validationBehavior="native"
+      onReset={() => {
+        setSenderAddress("");
+        setReceiverAddress("");
+        setAmount("");
+        setScheduleTime(new Time(0, 0));
+        setAction("reset");
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        let data = Object.fromEntries(new FormData(e.currentTarget));
+        setAction(`submit ${JSON.stringify(data)}`);
+      }}
+      className="m-5"
+    >
+      <Input
+        isRequired
+        errorMessage="Please enter a valid address"
+        label="Sender's Address"
+        labelPlacement="outside"
+        name="senderAddress"
+        placeholder="Enter your Wallet Address"
+        type="text"
+        value={senderAddress}
+        onChange={(e) => setSenderAddress(e.target.value)}
+      />
 
-            setAction(`submit ${JSON.stringify(data)}`);
-          }}
-        >
-          <Input
-            isRequired
-            errorMessage="Please enter a valid address"
-            label="Sender's Address"
-            labelPlacement="outside"
-            name="senderAddress"
-            placeholder="Enter your Wallet Address"
-            type="text"
-          />
+      <Input
+        isRequired
+        errorMessage="Please enter a valid address"
+        label="Receiver's Address"
+        labelPlacement="outside"
+        name="receiverAddress"
+        placeholder="Enter your receiver's address"
+        type="text"
+        value={receiverAddress}
+        onChange={(e) => setReceiverAddress(e.target.value)}
+      />
 
-          <Input
-            isRequired
-            errorMessage="Please enter a valid address"
-            label="Receiver's Address"
-            labelPlacement="outside"
-            name="receiverAddress"
-            placeholder="Enter your receiver's address"
-            type="text"
-          />
+      <Input
+        isRequired
+        errorMessage="Please enter a valid amount"
+        label="Amount"
+        labelPlacement="outside"
+        name="amount"
+        placeholder="Enter amount"
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
 
-          <Input
-            isRequired
-            errorMessage="Please enter a valid amount"
-            label="Amount"
-            labelPlacement="outside"
-            name="amount"
-            placeholder="Enter amount"
-            type=""
-          />
-          <TimeInput isRequired label="Schedule Time" />
-          <div className="flex gap-2">
-            <Button color="primary" type="submit">
-              Submit
-            </Button>
-            <Button type="reset" variant="flat">
-              Reset
-            </Button>
-          </div>
-        </Form>
-      </CardBody>
-    </Card>
+      <TimeInput
+        isRequired
+        label="Schedule Time"
+        value={scheduleTime}
+        onChange={setScheduleTime}
+      />
+
+      <div className="flex gap-2">
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
+        <Button type="reset" variant="flat">
+          Reset
+        </Button>
+      </div>
+    </Form>
   );
-}
+};
+
+export default TransferForm;
