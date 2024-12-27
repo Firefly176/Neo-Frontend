40ac478822580cc189fbd27fdb42134f09e937b7
Aniket Gupta <aniket17112000@gmail.com>
2024-12-28 01:54:13 +0530

    feat : added store wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { NextUIProvider } from "@nextui-org/react";
+import NavbarComponent from "./components/Navbar/Navbar.jsx";
+import StoreWrapper from "./store/StoreWrapper.jsx";
+import store from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
-      <App />
+      <StoreWrapper store={store}>
+        <NavbarComponent />
+        <App />
+      </StoreWrapper>
     </NextUIProvider>
   </StrictMode>,
 );
