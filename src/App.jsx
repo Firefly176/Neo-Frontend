ffbc9d4a2ac4a3a24272ac17fdc8f0ca58e0c60c
Aniket Gupta <aniket17112000@gmail.com>
2024-12-29 15:29:53 +0530

    feat : added navbar in router allowing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import Footer from "./components/Footer/Footer";
+import NavbarComponent from "./components/Navbar/Navbar";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
+      path: "/home",
+      element: (
+        <>
+          <NavbarComponent />
+          <Home />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/home",
+      element: (
+        <>
+          <NavbarComponent />
+          <Home />
+          <Footer />
+        </>
+      ),
     },
   ]);
 
