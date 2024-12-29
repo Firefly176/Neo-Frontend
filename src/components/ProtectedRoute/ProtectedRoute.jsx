55ea38e1a6bb463954d40be63e9cb172dd7af368
Aniket Gupta <aniket17112000@gmail.com>
2024-12-29 18:48:03 +0530

    feat : added protected route wrapper component

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,54 @@
+// ProtectedRoute.js
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { disconnect, setUserDetails } from "../../store/reducer";
+import { get } from "../../utils/api_helper";
+// import { toast } from "react-toastify";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+// eslint-disable-next-line react/prop-types
+const ProtectedRoute = ({ element }) => {
+  const userToken = useSelector((state) => state.auth.userToken);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.userDetails);
+
+  useEffect(() => {
+    if (!userToken) {
+      //   toast.error("Please login to proceed!");
+      navigate("/landing");
+    }
+  }, [userToken, navigate]);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await get("/auth/profile");
+        dispatch(setUserDetails(response));
+      } catch (error) {
+        console.error("Error fetching profile", error);
+        localStorage.clear();
+        dispatch(disconnect());
+        navigate("/landing");
+      }
+    };
+    fetchProfile();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {element}
+      <Footer />
+    </>
+  );
+};
+
+export default ProtectedRoute;
