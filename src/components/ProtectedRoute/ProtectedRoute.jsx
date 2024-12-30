7975da69cb0a1ab311085a50b35e67cd3e5ff6de
Aniket Gupta <aniket17112000@gmail.com>
2024-12-31 02:20:14 +0530

    feat : removed unnecessary code

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 // ProtectedRoute.js
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { disconnect, setUserDetails } from "../../store/reducer";
 import { get } from "../../utils/api_helper";
@@ -9,17 +9,8 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 // eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ element }) => {
-  // const userToken = useSelector((state) => state.auth.userToken);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.auth.userDetails);
-
-  // useEffect(() => {
-  //   if (!user) {
-  //     //   toast.error("Please login to proceed!");
-  //     navigate("/landing");
-  //   }
-  // }, [user]);
 
   useEffect(() => {
     const fetchProfile = async () => {
