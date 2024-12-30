dcc22b2d3a721c690b967ea59c0887d518f9dc69
Aniket Gupta <aniket17112000@gmail.com>
2024-12-30 21:25:44 +0530

    fix : .jsx extension

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Landing from "./pages/Landing";
-import Footer from "./components/Footer/Footer";
-import NavbarComponent from "./components/Navbar/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import Home from "./pages/Home.jsx";
+import Landing from "./pages/Landing.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import NavbarComponent from "./components/Navbar/Navbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
 
 function App() {
   const router = createBrowserRouter([
