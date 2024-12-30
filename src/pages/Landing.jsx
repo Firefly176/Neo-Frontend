8a480bbadb924395051db3e55ba2f598949552a4
Aniket Gupta <aniket17112000@gmail.com>
2024-12-31 02:23:46 +0530

    feat : imported into landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,12 @@
+import Hero from "../components/Hero/Hero";
+import Features from "../components/Features/Features";
 const Landing = () => {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
 };
 
 export default Landing;
